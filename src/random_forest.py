b201e54f60a6601edaf7e25ce80af51373212e3d
Xander Stovall <stovallx@gmail.com>
2026-06-30 19:11:32 -0400

    Add Random Forest

---
+++ src/random_forest.py
@@ -0,0 +1,17 @@
+from tune import tune_model
+from constants import SEED
+from utils import evaluate
+from sklearn.ensemble import RandomForestClassifier
+
+rf = RandomForestClassifier(random_state=SEED)
+
+RF_PARAM_GRID = {
+    "n_estimators": [100, 200, 300],
+    "max_depth": [3, 5, 7, None],
+    "min_samples_split": [2, 5, 10],
+    "min_samples_leaf": [1, 2, 4]
+}
+
+best_rf, best_params, best_auc = tune_model (rf, RF_PARAM_GRID)
+
+rf_results = evaluate(best_rf)
